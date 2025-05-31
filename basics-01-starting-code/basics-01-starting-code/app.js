1a15c3a4ba018137e49dd000ecf16814a926f245
Pedryn <franciscopeedroficial@gmail.com>
2025-05-31 08:52:55 -0300

    Section 2 is one-third complete

---
+++ basics-01-starting-code/basics-01-starting-code/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: 'Hello Vue!',
+      vueLink: 'https://vuejs.org/'
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
+}});
+
+app.mount('#user-goal');
