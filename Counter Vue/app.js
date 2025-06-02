2c30b20190e00ed96763ed2f72923c4327d84271
franciscopeedro <franciscopedroficial@gmail.com>
2025-06-01 23:08:22 -0300

    Counter 70%

--- Counter Vue/app.js
+++ Counter Vue/app.js
@@ -2,7 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
+  },
+  methods: {
+    setName(event){
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+    reset() {
+      this.counter = 0;
+    },
+    setName(event) {
+      this.name = event.target.value;
+    }
   },
 });
 
