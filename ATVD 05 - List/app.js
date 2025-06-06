6b86237840dcbb94eb7cb6d4ed48bfb42ab358c6
franciscopeedro <franciscopedroficial@gmail.com>
2025-06-06 17:19:53 -0300

    Challenge 5 completed.

---
+++ ATVD 05 - List/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            tasks: [],
+            enteredValue: '',
+            tasksListVisible: true
+        }
+    },
+    computed:{
+        buttonCaption() {
+            return this.tasksListVisible ? 'Hide List' : 'Show List';
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.enteredValue);
+            this.enteredValue = '';
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        },
+        toggleTaskList() {
+            this.tasksListVisible = !this.tasksListVisible;
+        }
+    }
+});
+
+app.mount('#assignment');
