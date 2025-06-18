259f532d4f6f01469667fd5b661c80be2e906c00
franciscopeedro <franciscopedroficial@gmail.com>
2025-06-18 20:02:28 -0300

    New feature: New Game and Surrender options. Controls are hidden when the match ends.

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -3,42 +3,56 @@ function getRandomInt(min, max) {
 }
 
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             playerHealth: 100,
             monsterHealth: 100,
             countRound: 0,
             winner: null
         };
     },
-    computed:{
+    computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { 
+                    width: '0%' 
+                };
+            }
             return { width: this.monsterHealth + '%' };
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
             return { width: this.playerHealth + '%' };
         },
         mayUseSpecialAttack() {
-            return this.countRound % 3 !== 0;
+            return this.countRound % 3 === 0;
         }
     },
     watch: {
         playerHealth(value) {
-            if (value <= 0 && this.monsterHealth < 0) {
-            this.winner = 'draw';
-            }else if (value <=0){   
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
                 this.winner = 'monster';
-                }
-            },
-        monsterHealth(value) {
-            if (value <= 0 && this.playerHealth < 0) {
-            this.winner = 'draw';
-            }else if (value <=0){   
-                this.winner = 'player';
-                }
             }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
     },
-    methods:{
+    methods: {
+        resetGame() {
+            this.playerHealth = 100,
+            this.monsterHealth = 100,
+            this.countRound = 0,
+            this.winner = null;
+        },  
         attackMonster() {
             this.countRound++;
             const damage = getRandomInt(5, 12);
@@ -63,8 +77,11 @@ const app = Vue.createApp({
                 this.playerHealth = 100;
             }
             this.attackPlayer();
+        },
+        surrender(){
+            this.winner = 'monster';
         }
-    },
+    }
 });
 
 app.mount('#game');
